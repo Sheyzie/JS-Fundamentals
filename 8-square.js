448d80eca9b619b156e5829d58c384e6c8a2a523
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-09-08 13:31:49 +0100

    add 8-square

---
+++ 8-square.js
@@ -0,0 +1,17 @@
+// process.argv property returns an array containing the command-line arguments
+const { argv } = require('node:process');
+
+const squareSize = parseInt(argv[2])
+
+if (isNaN(squareSize)) {
+    console.log('Missing size')
+} else {
+    let square = 'X'
+    for (let i = 0; i < squareSize; i++) {
+        square += 'X'
+    }
+
+    for (let i = 0; i < squareSize; i++) {
+        console.log(square)
+    }
+}
