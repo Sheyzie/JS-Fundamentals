3f18817360eb75c8f1cf7bfea6f0719907bc5861
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-09-08 13:05:23 +0100

    add 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+const multiLanguage = [
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing'
+]
+
+for(let i = 0; i < multiLanguage.length; i++) {
+    console.log(multiLanguage[i])
+}
